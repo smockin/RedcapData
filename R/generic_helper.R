952ea2f824a824d597d0f7a61aa4a29de2612723
Boniface Makone <BMakone@kemri-wellcome.org>
2014-12-02 11:33:13 +0300

    Added function to automatically open files in windowed os such as windows, ubuntu and mac

--- R/generic_helper.R
+++ R/generic_helper.R
@@ -2,3 +2,43 @@
 #' @import stringr
 #' @import RCurl
 NULL
+
+#' @name open_using_default_app
+#'
+#' @title Open file using  OS default application
+#'
+#' @description Pop a file open using the default app set at the operating system level to handle files of the type specified.
+#'
+#' @details This allows for results to be viewed interactively during analysis and programming.
+#'
+#' This is only applicable in GUI settings and should only be used in windowed environmnents.
+#'
+#' @param file_path Path to target file
+#'
+#' @export
+#'
+
+open_using_default_app = function(file_path) {
+  file_path = paste0("\"", file_path, "\"")
+
+  if(!file.exists(file_path))
+    stop("The file does not exist!")
+  if(Sys.info()["sysname"] == "Windows") {
+    shell.exec(file_path)
+  } else if (Sys.info()["sysname"] == "Linux") {
+    tryCatch({
+      system(paste("gnome-open", file_path, sep = " "), intern = FALSE)
+    }
+    , warning = function(w) warning(w$message)
+    , error = function(e) stop(e$message)
+    )
+  }  else {
+    tryCatch({
+      system(paste0("open", file_path, sep = " "), intern = FALSE)
+    }
+    , warning = function(w) warning(w$message)
+    , error = function(e) stop(e$message)
+    )
+  }
+  Sys.sleep(2)
+}
