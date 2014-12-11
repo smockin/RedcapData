ee120e3e84556ba4a9d2cb8323b1ba636de8a24b
Boniface Makone <BMakone@kemri-wellcome.org>
2014-12-11 09:02:25 +0300

    Updated previous data type check functions and added another for booleans

--- R/data_types.R
+++ R/data_types.R
@@ -1,14 +1,27 @@
 is_date = function(x) {
   x = as.character(x)
+  if (is.na(x) | str_trim(x) == "")
+    return(TRUE)
   isTRUE(regexpr("^[0-9]{4}[/\\-]{1}[0-9]{2}[/\\-]{1}[0-9]{2}$", str_trim(x)) > 0L)
 }
 
 is_int = function(x) {
   x = as.character(x)
+  if (is.na(x) | str_trim(x) == "")
+    return(TRUE)
   isTRUE(regexpr("^[\\-]?[0-9]*$", str_trim(x)) > 0L)
 }
 
 is_number = function(x) {
   x = as.character(x)
+  if (is.na(x) | str_trim(x) == "")
+    return(TRUE)
   isTRUE(regexpr("^[\\-]?[0-9]*[\\.]?[0-9]*$", str_trim(x)) > 0L)
+}
+
+is_boolean = function(x) {
+  x = as.character(x)
+  if (is.na(x) | str_trim(x) == "")
+    return(TRUE)
+  isTRUE(x %in% c("T", "F", "TRUE", "FALSE"))
 }
