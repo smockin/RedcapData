339497831c3fd9b74c851e7149eee7d06d3321dd
Boniface Makone <BMakone@kemri-wellcome.org>
2014-12-18 12:17:11 +0300

    Updated configuration helper functions and updated the documentation

--- R/redcap_config_helper.R
+++ R/redcap_config_helper.R
@@ -5,7 +5,7 @@ NULL
 #'
 #' @title Wrapper for creating REDCap update objects
 #'
-#' @description Cleaner way for instantiating REDCap update objects.
+#' @description Cleaner way for instantiating RedcapUpdate object(s).
 #'
 #' It uses an R data frame to instantiate update objects.
 #'
@@ -34,9 +34,7 @@ NULL
 #'
 #' @param updates_data A dataset containing the updates info. For the column specifications, see details.
 #'
-#' @export
-#'
-#' @seealso \code{\link{redcap_update}}
+#' @seealso \code{\link{RedcapUpdate}} \code{\link{Redcap}}, \code{\link{redcap_project}}
 #'
 #' @return A list of redcap update class objects.
 #'
@@ -74,7 +72,7 @@ load_updates = function(updates_data) {
     new_vars = na.omit(new_vars)[1]
     new_vars = str_trim(unlist(strsplit(new_vars, ";")))
     new_vars = unique(new_vars)
-    redcap_update$new(name = name, site_info = site_info, new_vars = new_vars)
+    RedcapUpdate$new(name = name, site_info = site_info, new_vars = new_vars)
   })
   value
 }
@@ -89,34 +87,79 @@ load_updates = function(updates_data) {
 #'
 #' This avoids messing up instantiation in R6 classes which is more complex and error prone.
 #'
-#' @details This function acts as a wrapper for instantiating an object that abstracts the REDCap updates.
+#' @details This function acts as a wrapper for instantiating an object that abstracts the REDCap configurations.
 #'
 #' It performs the necessary checks for instantiation hence avoids messy objects.
 #'
 #' It then calls the new method of the underlying reference class.
 #'
-#' The update dataset must contain the following variables:
+#' The configuration dataset must contain the following variables:
 #'
 #' \describe{
-#'  \item{update}{The update name of format mon-yy, eg Nov-14, Feb-13}
-#'  \item{site}{The hospital id of the site, must be integer and must be replicated for each update}
-#'  \item{date}{The date of the update for each specific site}
-#'  \item{new_vars}{
-#'  The new variables introduced during the update <must have at least one entry per update>.
+#'  \item{key}{The name of the configuration}
+#'  \item{value}{The value the configuration takes}
+#'  \item{type}{The data type for the configuration. Its either `string`, `number`, `integer`, `date` or `boolean`}
+#' }
 #'
-#'  The first non misssing entry will be used.
+#' Some common configuations include:
 #'
-#'  The variable names must be separated by semi-colons.
+#' \describe{
+#'  \item{api_url}{The url to the redcap api.
+#'
+#'  Just append /api/ to the REDCap instance's url link. [type : `string`].
+#'
+#'  Defaults to http://localhost/redcap/api/ if not specified.
+#'
+#'  }
+#'  \item{token}{Token necessary for authentication.
+#'
+#'  Must have api rights to obtain this from REDCap. [type : `string`].
+#'
+#'  Defaults to an empty string if not specified. This is invalid and therefore is the only configuration that must be specified.
+#'  }
+#'  \item{local}{Whether REDCap instance is local to the machine. [type: `boolean`].
+#'
+#'  Defaults to TRUE if not specified.
+#'  }
+#'  \item{chunked}{Whether to pull data from REDCap / Validate data entry in chunks. This helps if the data size is large or in cases of network latency [type: `boolean`].
+#'
+#'  Defaults to FALSE if not specified hence data operations are carried out in bulk by default.
+#'
+#'  }
+#'  \item{chunksize}{The size of the chunks to be used. Depends on the data size [type: `integer`].
+#'
+#'  This is ignored if chunked is FALSE but must be provided in case chunked is TRUE.
+#'
+#'  }
+#'  \item{date_var}{The name of the variable that holds the date of record entry [type: `string`].
+#'
+#'  This defaults to date_today in line with CIN's data governance framework if not specified.
+#'  }
+#'  \item{hosp_var}{The name of the variable that holds the date of record entry [type: `string`]
+#'
+#'  This defaults to hosp_id in line with CIN's data governance framework if not specified.
+#'  }
+#'  \item{report_location}{The path to the generated error report [type: `string`]
+#'
+#'  This defaults to a temporary file in a subdirectory of the default temporary directory if not specified.
+#'  }
+#'  \item{hosp_to_validate}{The id of the hospital to generate an error report on. [type: `integer`].
+#'
+#'  If entry does not match this code an error is raised.
+#'
+#'  This defaults to NA if not specified.
 #'  }
 #' }
 #'
-#' @param updates_data A dataset containing the updates info. For the column specifications, see details.
+#' @param config_data A dataset containing the configurations. For the column specifications and some common entries, see details.
+#' @param custom_code A character vector containing custom code for error reporting. If located in a file use readLines to load the contents to a character vector
+#' @param exclusion_pattern A character vector containing regex patterns for variables to be excluded from error reporting.
 #'
-#' @export
+#' This may be because they are handled specially in the custom code or do not add value to the error reporting process.
 #'
-#' @seealso \code{\link{redcap_update}}
+#' @seealso \code{\link{RedcapUpdate}}, \code{\link{Redcap}}, \code{\link{redcap_project}}
 #'
-#' @return A list of redcap update class objects.
+#' @return A RedcapConfig object.
 #'
 
 load_configs = function(config_data = NULL, custom_code = NA, exclusion_pattern = NA) {
@@ -124,7 +167,7 @@ load_configs = function(config_data = NULL, custom_code = NA, exclusion_pattern 
   if (!is.null(config_data)) {
     if (all(!is.data.frame(config_data) | is.matrix(config_data)))
       stop("invalid config data")
-    config_data = as.data.frame(config_data)
+    config_data = as.data.frame(config_data, stringsAsFactors = TRUE)
     if (!all(c("key", "value", "type") %in% names(config_data)))
       stop("config data should have key, value and data type")
     config_data = config_data[, c("key", "value", "type")]
@@ -133,13 +176,13 @@ load_configs = function(config_data = NULL, custom_code = NA, exclusion_pattern 
     config_data = config_data[config_data$key %in% names(config_keys),]
     config_data = data.frame(sapply(config_data, function(x) {
       str_trim(x)
-    }))
+    }), stringsAsFactors = TRUE)
     if (nrow(config_data) > 0) {
       invisible(sapply(config_data$key, function(k) {
         cnf = config_data[config_data$key == k,]
-        ky = cnf$key
-        typ = cnf$type
-        val = cnf$value
+        ky = as.character(cnf$key)
+        typ = as.character(cnf$type)
+        val = as.character(cnf$value)
         val = if (typ == "date") {
           if (str_trim(val) %in% c("NA", "")) {
             as.Date(NA)
@@ -147,41 +190,50 @@ load_configs = function(config_data = NULL, custom_code = NA, exclusion_pattern 
             stop(paste0(sQuote(ky), " must be a date (format:yyyy-mm-dd)"))
           }
           as.Date(val)
-        } else if (typ == "float") {
+        } else if (typ == "number") {
           if (str_trim(val) %in% c("NA", "")) {
-            val = NA_real_
+            NA_real_
           } else if (!is_number(val)) {
             stop(paste0(sQuote(ky), " must be a float"))
           }
           as.numeric(val)
-        } else if (type == "integer") {
+        } else if (typ == "integer") {
           if (str_trim(val) %in% c("NA", "")) {
-            val = NA_integer_
-          } else if (!is_integer(val)) {
+            NA_integer_
+          } else if (!is_int(val)) {
             stop(paste0(sQuote(ky), " must be an integer"))
           }
           as.integer(val)
-        } else if (type == "boolean") {
+        } else if (typ == "boolean") {
           if (str_trim(val) %in% c("NA", "")) {
-            val = NA
+            NA
           } else if (!is_boolean(val)) {
             stop(paste0(sQuote(ky), " must be a boolean"))
           }
           as.logical(val)
+        } else if (typ == "string") {
+          if (str_trim(val) %in% c("NA", ""))
+            NA_character_
+          else
+            as.character(val)
+        } else {
+          stop("config data type not supported!. must be either `string`, `date`, `integer`, `number` or `boolean`")
         }
-        tmp[[k]] = val
+        tmp = get("tmp", envir = parent.frame(n = 3))
+        tmp[[ky]] = val
+        tmp = assign("tmp", tmp, envir = parent.frame(n = 3))
       }))
     }
   }
   if (!is.na(custom_code))
     if (!is.character(custom_code))
-      stop("invaid custom code")
+      stop("invalid custom code")
   if (!is.na(exclusion_pattern))
     if (!is.character(exclusion_pattern))
       stop("invalid exclusion pattern")
   tmp$custom_code = custom_code
   tmp$exclusion_pattern = exclusion_pattern
-  obj = redcap_config$new(config = as.environment(tmp))
+  obj = RedcapConfig$new(configs = as.environment(tmp))
   if (!obj$is_valid())
     stop("configs not set")
   obj
@@ -190,10 +242,11 @@ load_configs = function(config_data = NULL, custom_code = NA, exclusion_pattern 
 config_keys = list(
   api_url = "http://localhost/redcap/api/",
   token = "",
-  local = "true",
+  local = TRUE,
   chunked = FALSE,
-  chunksize = NA,
+  chunksize = NA_integer_,
   hosp_var = "hosp_id",
   date_var = "date_today",
-  hosp_code = NA
+  report_location = tempfile("Error_Report", fileext = ".csv"),
+  hosp_to_validate = NA_integer_
   )
